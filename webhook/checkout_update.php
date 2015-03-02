6149c421037292c3e0bf1a9df4b936e9db8df5db
jason-footing-cko <jason.footing@checkout.com>
2015-03-02 10:56:41 +0400

    webhook

--- webhook/checkout_update.php
+++ webhook/checkout_update.php
@@ -1,58 +1,33 @@
 <?php
 
+include 'autoload.php';
 if ($_POST['submit']) {
 
     //set url
-    $post_url = 'https://api2.checkout.com/v1/charges/'.$_POST['charge_id'];
+    $post_url = 'http://preprod.checkout.com/api.gw3/v2/charges/'.$_POST['charge_id'];
     $url = $_POST['url'];
-
-    $post_data = array(
-        'description' => $_POST['url'],
-        'metadata' => array (
-            'code key'=> 'Test updated',
-        )
-    );
 }
 
-$data_string = json_encode($post_data);
+$Api = new CheckoutApi_Api();
+
+
+
+// get charge
 $request = curl_init($post_url);
-curl_setopt($request, CURLOPT_CUSTOMREQUEST, "PUT");
-curl_setopt($request, CURLOPT_HTTPHEADER, array('Content-Type:application/json;charset=UTF-8', 'Authorization:sk_test_2381d84e-6a1b-49bf-86c9-e4840c7d0f28'));
+curl_setopt($request, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($request, CURLOPT_HTTPHEADER, array('Content-Type:application/json;charset=UTF-8', 'Authorization:sk_test_8655e7cb-d031-4444-802b-388d8f96820c'));
 curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($request, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($request, CURLOPT_SSL_VERIFYPEER, FALSE);
 $post_response = curl_exec($request);
 curl_close($request);
-//var_dump($post_response);
 
-$data_string = json_encode($post_response);
+//post charge to web hook
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_response);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-$post_response = curl_exec($ch);
+$response = curl_exec($ch);
+curl_close($ch );;
 
 //$input = json_decode($post_response, true);
-
-
-//echo '<form id="form_cartid">';
-//foreach ($input as $key => $value) {
-//    if (!is_array($value)) {
-//        echo '<label>'. $key.' :</label><input type="text" name="'.$key.'" value="'. $value.'"/><br />';
-//    }
-//    else {
-//        foreach ($value as $k => $data) {
-//            if (!is_array($data)) {
-//                echo '<label>'. $k.' :</label><input type="text" name="'.$k.'" value="'. $data.'"/><br />';;
-//            }
-//            else {
-//                foreach($data as $key_data => $string) {
-//                    echo '<label>'. $key_data.' :</label><input type="text" name="'.$key_data.'" value="'. $string.'"/><br />';;
-//                }
-//            }
-//        }
-//    }
-//};
-//echo '</form>';
--- webhook/posturl.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content  = file_get_contents('php://input');
-$input = json_decode($content, true);
-var_dump($input);
-
