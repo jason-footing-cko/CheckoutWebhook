ef85cbb8051d410d84bf2001e92acfa409a78d57
jason-footing-cko <jason.footing@checkout.com>
2015-03-24 14:35:58 +0400

    webhook drupal commerce

--- Shopping_Cart/Abante/extensions/checkoutapipayment/Model/Methods/creditcard.php
+++ Shopping_Cart/Abante/extensions/checkoutapipayment/Model/Methods/creditcard.php
@@ -32,30 +32,30 @@ class Model_Methods_creditcard extends Model_Methods_Abstract implements Model_I
 
         $toReturn = array(
             'text_card_details' => $this->language->get('text_card_details'),
-            'text_wait' => $this->language->get('text_wait'),
-            'order_email' => $order_info['email'],
-            'order_currency' => $this->currency->getCode(),
-            'amount' => (int) $order_info['total'] * 100,
-            'publicKey' => $this->config->get('checkoutapipayment_public_key'),
-            'email' => $order_info['email'],
-            'name' => $order_info['firstname'] . ' ' . $order_info['lastname'],
-            'store_name' => $order_info['store_name'],
-            'paymentToken' => $paymentTokenArray['token'],
-            'message' => $paymentTokenArray['message'],
-            'success' => $paymentTokenArray['success'],
-            'eventId' => $paymentTokenArray['eventId'],
-            'textWait' => $this->language->get('text_wait'),
+            'text_wait'         => $this->language->get('text_wait'),
+            'order_email'       => $order_info['email'],
+            'order_currency'    => $this->currency->getCode(),
+            'amount'            => (int) $order_info['total'] * 100,
+            'publicKey'         => $this->config->get('checkoutapipayment_public_key'),
+            'email'             => $order_info['email'],
+            'name'              => $order_info['firstname'] . ' ' . $order_info['lastname'],
+            'store_name'        => $order_info['store_name'],
+            'paymentToken'      => $paymentTokenArray['token'],
+            'message'           => $paymentTokenArray['message'],
+            'success'           => $paymentTokenArray['success'],
+            'eventId'           => $paymentTokenArray['eventId'],
+            'textWait'          => $this->language->get('text_wait'),
         );
 
         $toReturn['back'] = HtmlElementFactory::create(array('type' => 'button',
-                    'name' => 'back',
-                    'text' => $this->language->get('button_back'),
+                    'name'  => 'back',
+                    'text'  => $this->language->get('button_back'),
                     'style' => 'button',
-                    'href' => $back
+                    'href'  => $back
         ));
         $toReturn['submit'] = HtmlElementFactory::create(array('type' => 'button',
-                    'name' => 'checkoutapipayment_button',
-                    'text' => $this->language->get('button_confirm'),
+                    'name'  => 'checkoutapipayment_button',
+                    'text'  => $this->language->get('button_confirm'),
                     'style' => 'button'
         ));
 
@@ -101,9 +101,9 @@ class Model_Methods_creditcard extends Model_Methods_Abstract implements Model_I
         foreach ($productsLoad as $item) {
 
             $products[] = array(
-                'name' => $item['name'],
-                'sku' => $item['key'],
-                'price' => $this->currency->format($item['price'], $this->currency->getCode(), false, false),
+                'name'     => $item['name'],
+                'sku'      => $item['key'],
+                'price'    => $this->currency->format($item['price'], $this->currency->getCode(), false, false),
                 'quantity' => $item['quantity']
             );
         }
@@ -111,31 +111,32 @@ class Model_Methods_creditcard extends Model_Methods_Abstract implements Model_I
         $billingAddressConfig = array(
             'addressLine1' => $order_info['payment_address_1'],
             'addressLine2' => $order_info['payment_address_2'],
-            'postcode' => $order_info['payment_postcode'],
-            'country' => $order_info['payment_iso_code_3'],
-            'city' => $order_info['payment_city'],
-            'phone' => $order_info['telephone'],
+            'postcode'     => $order_info['payment_postcode'],
+            'country'      => $order_info['payment_iso_code_3'],
+            'city'         => $order_info['payment_city'],
+            'phone'        => $order_info['telephone'],
         );
 
         $shippingAddressConfig = array(
-            'addressLine1' => $order_info['shipping_address_1'],
-            'addressLine2' => $order_info['shipping_address_2'],
-            'postcode' => $order_info['shipping_postcode'],
-            'country' => $order_info['shipping_iso_code_3'],
-            'city' => $order_info['shipping_city'],
-            'phone' => $order_info['telephone'],
+            'addressLine1'  => $order_info['shipping_address_1'],
+            'addressLine2'  => $order_info['shipping_address_2'],
+            'postcode'      => $order_info['shipping_postcode'],
+            'country'       => $order_info['shipping_iso_code_3'],
+            'city'          => $order_info['shipping_city'],
+            'phone'         => $order_info['telephone'],
             'recipientName' => $order_info['firstname'] . ' ' . $order_info['lastname']
         );
 
         $config['postedParam'] = array_merge($config['postedParam'], array(
-            'email' => $order_info['email'],
-            'value' => $amountCents,
-            'currency' => $this->currency->getCode(),
-            'description' => 'Order number::'. $orderId,
+            'email'           => $order_info['email'],
+            'trackId'         => $orderId,
+            'value'           => $amountCents,
+            'currency'        => $this->currency->getCode(),
+            'description'     => 'Order number::'. $orderId,
             'shippingDetails' => $shippingAddressConfig,
-            'products' => $products,
-            'metadata' => array('trackId' => $orderId),
-            'billingDetails' => $billingAddressConfig
+            'products'        => $products,
+            'metadata'        => array('trackId' => $orderId),
+            'billingDetails'  => $billingAddressConfig
         ));
 
         $Api = CheckoutApi_Api::getApi(array('mode' => $this->config->get('checkoutapipayment_mode')));
